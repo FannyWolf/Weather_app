a5b33f494a5b8554c106e2db635989ba89197a80
Liubov Korchashkina <korchashkina.liubov@gmail.com>
2024-08-05 09:45:35 +0200

    new weather app

--- src/features/products/weatherAction.ts
+++ src/features/products/weatherAction.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 export const getWeather = createAsyncThunk(
   'getWeather',
-  async (_, thunkAPI) => {
+  async (city: string, thunkAPI) => {
     try {
-      const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=95abbc8a327ef422700ea93c6cee52f3');
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=95abbc8a327ef422700ea93c6cee52f3`);
       return response.data;
     } catch (error: any) {
       return thunkAPI.rejectWithValue(error.message);
